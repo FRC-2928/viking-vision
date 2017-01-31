3bd874638f85c57da0007bed10ad8c508046b151
Sam Burns <samburns157@gmail.com>
2017-01-31 03:46:32 +0000

    Made it headless, added networktables

--- viking-vision.py
+++ viking-vision.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python2
 import cv2
-import cv2.xfeatures2d
 import sys
 import numpy as np
+from networktables import NetworkTables
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+ip = "10.29.28.2"
+
+def ntInit(table):
+    NetworkTables.initialize(ip)
+    return NetworkTables.getTable(table)
 
 # From http://stackoverflow.com/questions/4961017/clojure-style-function-threading-in-python
 def T(*args):
@@ -17,31 +26,35 @@ def brightPass(src):
 def blur(src):
     return cv2.GaussianBlur(src, (13, 13), 40)
 
-'''def blobbify(src):
+def blobbify(src):
     data = src.flat
     dims = src.shape
     print dims
     for i in range(0, len(data)):
         if data[i] > 1:
             cv2.ellipse(src, (i % dims[0], i // dims[0]), (15, 6), 90, 0, 360, 0xFF, -1)
-    return src'''
+    return src
 
 def outline(src):
     dst = np.zeros(src.shape, np.uint8)
     im2, contours, hierarchy  = cv2.findContours(src, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(dst, contours, -1, (255), 1)
-    return dst
+    return dst, contours
 
 def main(camera = 0):
-    filterPipeline = (toGrayscale, brightPass, blur, outline)
+    vc = ntInit('VisionControl')
     ret = True
     cap = cv2.VideoCapture(camera)
-    cv2.namedWindow("Output")
+    #cv2.namedWindow("Output")
     while ret:
         ret, frame = cap.read()
-        cv2.imshow("Output", T(frame, *filterPipeline)) 
+        frame, contours = outline(T(frame, toGrayscale, brightPass, blur))
+        print len(contours)
+
+        vc.putNumber("frameSum", frame.sum()/255)
+        print frame.sum()/255
         if(cv2.waitKey(30) & 0xFF == ord('q')):
             break
     cap.release()
-
-main()
+if __name__ == "__main__":
+    main()
